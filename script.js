30251ba6a570c82294980fa9e9d2c82bfe298e68
Ha1f_Deaf <jaredtspears@gmail.com>
2018-09-27 22:16:14 -0400

    added styling, fixed MQ's, added content for top 4 projects. Need to finish firebase form (may not do that but it is an option).

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 
+// keys
+// var k = require(".config.js");
+
 // sQuery for card hovor dimmer for card
 $('.special.cards .image').dimmer({
     on: 'hover'
   });
 
+  // function for the anchor for the connect tab to animate down to the connect form
+  $(document).ready(function){
+    $("a.scrollLink").click(function(event){
+      event.preventDefault();
+      $("html.body").animate({scrollTop: $($(this).
+        attr("href")).offset().top}, 500);
+    })
+  }
 
 //   ===== may need firebase to collect data from inputs ==========
 
  // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyBEtRKXASlutE5E_toOPOS6jvGJ4i-06ns",
-    authDomain: "jared-spears-portfolio.firebaseapp.com",
-    databaseURL: "https://jared-spears-portfolio.firebaseio.com",
-    projectId: "jared-spears-portfolio",
-    storageBucket: "",
-    messagingSenderId: "932459567542"
-  };
-  firebase.initializeApp(config);
+//  var config = {
+//     apiKey: fireBaseKey,                                     //you must create a .gitignore file for your key
+//     authDomain: "jared-spears-portfolio.firebaseapp.com",
+//     databaseURL: "https://jared-spears-portfolio.firebaseio.com",
+//     projectId: "jared-spears-portfolio",
+//     storageBucket: "",
+//     messagingSenderId: "932459567542"
+//   };
+//   firebase.initializeApp(config);
 
 //   have #submit ready for calling firebase
 
